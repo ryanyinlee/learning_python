33768e445c815b48975bd7cad4e1be93219e44d3
Ryan Lee <ryanyinlee@protonmail.com>
2023-03-06 00:59:34 -0800

    return values, optional args

---
+++ 08 - Functions/08-137-TIY-8-3.py
@@ -0,0 +1,8 @@
+#8-3 t-shirt
+def make_shirt(size, text):
+    print(f"We are printing a t-shirt in size {size} with a message that says {text}")
+
+make_shirt("XXL", "twist erase 0.5")
+
+
+make_shirt(size="smol", text="itsy bitsy")
