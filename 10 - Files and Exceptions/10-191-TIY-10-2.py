84b076f011ce9d1423dd9a7839a22100cea61e82
Ryan Lee <ryanyinlee@protonmail.com>
2023-03-12 03:35:13 -0700

    reading from files

--- 10 - Files and Exceptions/10-191-TIY-10-2.py
+++ 10 - Files and Exceptions/10-191-TIY-10-2.py
@@ -1,9 +1,9 @@
-#10-1 learning python
+#10-2 learning C
 
 filepath = '/Users/ryanlee/projects/python/10 - Files and Exceptions/text/learningpython.txt'
 
 with open(filepath) as file_object:
     for line in file_object:
+        line = line.replace('Python', 'C')
         print(line.rstrip())
-        print(line.rstrip())
-        print(line.rstrip())
+
